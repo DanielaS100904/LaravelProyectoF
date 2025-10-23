8bfa84a5538a44aaa203dbdabe404b52cc721cb8
DanielaS100904 <160405376+DanielaS100904@users.noreply.github.com>
2025-10-22 22:10:48 -0500

    Add files via upload

---
+++ lang/es/validation.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'accepted' => 'El campo :attribute debe ser aceptado.',
+    'active_url' => 'El campo :attribute no es una URL válida.',
+    'after' => 'El campo :attribute debe ser una fecha posterior a :date.',
+    'alpha' => 'El campo :attribute solo puede contener letras.',
+    'alpha_num' => 'El campo :attribute solo puede contener letras y números.',
+    'array' => 'El campo :attribute debe ser un conjunto.',
+    'before' => 'El campo :attribute debe ser una fecha anterior a :date.',
+    'between' => [
+        'numeric' => 'El campo :attribute debe estar entre :min y :max.',
+        'file' => 'El archivo :attribute debe pesar entre :min y :max kilobytes.',
+        'string' => 'El campo :attribute debe tener entre :min y :max caracteres.',
+        'array' => 'El campo :attribute debe tener entre :min y :max elementos.',
+    ],
+    'boolean' => 'El campo :attribute debe ser verdadero o falso.',
+    'confirmed' => 'La confirmación de :attribute no coincide.',
+    'date' => 'El campo :attribute no es una fecha válida.',
+    'email' => 'El campo :attribute debe ser una dirección de correo válida.',
+    'max' => [
+        'string' => 'El campo :attribute no puede tener más de :max caracteres.',
+    ],
+    'min' => [
+        'string' => 'El campo :attribute debe tener al menos :min caracteres.',
+    ],
+    'required' => 'El campo :attribute es obligatorio.',
+    'unique' => 'El campo :attribute ya está en uso.',
+    'attributes' => [],
+];
